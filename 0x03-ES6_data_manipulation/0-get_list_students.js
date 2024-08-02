e389778551d6ad861c5f7e70b86d283af9c53bb4
NyuydineBill <billleynyuy@gmail.com>
2024-08-02 23:30:44 +0100

    new taskkkkkkkkkkks done

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,5 @@
-Create a function named getListStudents that returns an array of objects. function getListStudents() {
+//Create a function named getListStudents that returns an array of objects. 
+function getListStudents() {
   const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
