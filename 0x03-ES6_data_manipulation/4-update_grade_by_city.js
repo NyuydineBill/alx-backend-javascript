83af5d5e138ee28d4c26c6437f76ceb1559e61d7
NyuydineBill <billleynyuy@gmail.com>
2024-08-02 23:37:11 +0100

    update

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,33 +1,30 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-  if (!Array.isArray(students)) {
+/**
+ * Updates the grades of students from a specific city.
+ *
+ * @param {Object[]} students - An array of student objects.
+ * @param {string} city - The city to filter students by.
+ * @param {Object[]} newGrades - An array of new grade objects.
+ * @returns {Object[]} An array of student objects with updated grades.
+ */
+function updateStudentGradesByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
-  if (!Array.isArray(newGrades)) {
-    return [];
-  }
 
-  const stCity = students.filter((student) => student.location === city);
+  const studentsInCity = students.filter(
+    (student) => student.location === city
+  );
 
-  const studentsGraded = stCity.map((student) => {
-    const gradeFilter = newGrades.filter(
-      (newGrade) => newGrade.studentId === student.id,
-    );
-
-    let grade;
-
-    if (gradeFilter[0]) {
-      grade = gradeFilter[0].grade;
-    } else {
-      grade = 'N/A';
-    }
+  const updatedStudents = studentsInCity.map((student) => {
+    const newGrade = newGrades.find((grade) => grade.studentId === student.id);
 
     return {
       ...student,
-      grade,
+      grade: newGrade && newGrade.grade ? newGrade.grade : "N/A",
     };
   });
 
-  return studentsGraded;
+  return updatedStudents;
 }
 
-export default updateStudentGradeByCity;
+export default updateStudentGradesByCity;
